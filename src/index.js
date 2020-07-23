2c1efd5696e89ac3d94df81fc41ff15ca197504d
Waqas Noor <waqas2c@yahoo.com>
2020-07-23 23:01:37 +0500

    Gsap Getting Started

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("helloworld");
+import gsap from "gsap";
+
+gsap.to(".logo", {
+  x: 300,
+  duration: 2,
+  backgroundColor: "#560563",
+  borderRadius: "20%",
+  border: "1px solid white",
+  y: 250,
+});
