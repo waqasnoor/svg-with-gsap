f899f7432eb39e699c7fae9790c824d8f7c663a8
Waqas Noor <waqas2c@yahoo.com>
2020-07-28 23:55:01 +0500

    chainig and add label

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import gsap from "gsap";
 const tl = gsap.timeline({ repeat: 30, repeatDelay: 1 });
-
-tl.add(
-  gsap.to(".green", { scale: 0.5, y: 200, stagger: 0.5, ease: "elastic" })
-);
-tl.add(
-  gsap.to(".orange", { scale: 0.5, y: 200, stagger: 0.5, ease: "bounce" })
-);
-tl.add(
-  gsap.to(".blue", {
-    duration: 0.6,
-    scale: 0.5,
-    y: 200,
-    ease:
-      "rough({ template: none.out, strength: 1, points: 20, taper: 'none', randomize: true, clamp: false})",
-  })
-);
+tl.to(".green", {
+  scale: 0.5,
+  y: 200,
+  stagger: 0.5,
+  ease: "elastic",
+})
+  .addLabel("orangeAndBlue")
+  .to(
+    ".orange",
+    { scale: 0.5, y: 200, stagger: 0.5, ease: "bounce" },
+    "orangeAndBlue"
+  )
+  .to(
+    ".blue",
+    {
+      duration: 0.5,
+      scale: 0.5,
+      y: 200,
+      ease:
+        "rough({ template: none.out, strength: 1, points: 20, taper: 'none', randomize: true, clamp: false})",
+    },
+    "orangeAndBlue"
+  );
