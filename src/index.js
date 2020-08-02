b4e9938c36cc7474e7a0040b7c86026c37105d5f
Waqas Noor <waqas2c@yahoo.com>
2020-08-02 20:32:24 +0500

    practicing with motion path plugin

--- src/index.js
+++ src/index.js
@@ -2,29 +2,8 @@ import gsap from "gsap";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 gsap.registerPlugin(MotionPathPlugin);
-const tl = gsap.timeline({ repeat: 30, repeatDelay: 1 });
-tl.timeScale(0.5);
-tl.to(".green", {
-  scale: 0.5,
-  y: 200,
-  stagger: 0.5,
-  ease: "elastic",
-  rotation: 60,
-})
-  .addLabel("orangeAndBlue")
-  .to(
-    ".orange",
-    { scale: 0.5, y: 200, stagger: 0.5, ease: "bounce", rotation: 120 },
-    "orangeAndBlue"
-  )
-  .to(
-    ".blue",
-    {
-      duration: 0.5,
-      scale: 0.5,
-      y: 200,
-      ease: "bounce",
-      rotation: 180,
-    },
-    "orangeAndBlue"
-  );
+
+gsap.to("circle", {
+  duration: 5,
+  motionPath: "m119.5,260.05c219,-4 27,-220 282,-7c255,213 374,-63 296,-66",
+});
